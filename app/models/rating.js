871057bb3550f0e6adbe897c1e9e45f46f8aafed
AndreAvet <Andre@Andres-MBP.csusm.edu>
2016-10-24 10:41:51 -0700

    Added schemas to handle drinks and rating and updated user schema

---
+++ app/models/rating.js
@@ -0,0 +1,26 @@
+// Import our requirements.
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+// Declare a Mongoose Schema.
+// Tells mongodb what values it will store.
+var RatingSchema = new Schema({
+	drinksRating: {
+		type: var, //add drinks into a drink array
+		required: true,
+		index: {
+			unique: true
+		}
+
+	drinkNames : {
+		type: var, //add drink names into this one
+		required: true
+	},
+
+
+	}
+
+});
+
+module.exports = mongoose.model('Rating', RatingSchema);
