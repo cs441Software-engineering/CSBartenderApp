795216bf1f5928bfa4100fd87ee3baf2b82c49df
jaffer <eriklab1222@yahoo.com>
2016-11-01 21:18:44 -0700

    uncommented a line

--- app/routes/api.js
+++ app/routes/api.js
@@ -119,7 +119,7 @@ module.exports = function(app, express) {
 	apiRouter.get('/getDrinkBySearch/:ingName', function (req, res) {
 		drinkApi.getDrinkQuickSearch(req.params.ingName, function(data, error) {
 				// console.log(data);
-				//drinkApi.addIngredient(data);//adds ingredient
+				drinkApi.addIngredient(data);//adds ingredient
 				drinkApi.getIngredientsForDrink(data);
 				res.json({
 					success:true,
